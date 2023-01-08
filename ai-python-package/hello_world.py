354dedcf51bfc59a19e1e8e45374c25619ec1bcd
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-08 16:27:57 -0500

    Initialize dciborow/black-attack

---
+++ ai-python-package/hello_world.py
@@ -0,0 +1,14 @@
+def hello_world(i: int = 0) -> str:
+    "Doc Stringz"
+    print("hello world")
+
+
+def good_night():
+    print("good night")
+    x = 2
+    return
+
+
+def hello_goodbye():
+    hello_world("x")
+    good_night()
